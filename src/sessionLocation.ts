7ddd7286686bebab89bc989579ff2066c517cd7a
Saurabh Patel <saurabhp@masteroapp.com>
2022-10-11 19:21:22 +0530

    feat(sessionlocation): simplified sessionLoaction body

--- src/sessionLocation.ts
+++ src/sessionLocation.ts
@@ -6,8 +6,7 @@ export const getRequestIpAddress = requestIp.mw()
 
 export const setSessionLocation = async (req: Request, res: Response, next: NextFunction) => {
   const ip: string | undefined = req.clientIp;
-  const geoLocation: any = await geoip.lookup(ip!);
-  if (geoLocation) geoLocation.coOrdinates = { longtitude: geoLocation?.ll[0], latitude: geoLocation?.ll[1] };
+  const geoLocation = await geoip.lookup(ip!);
   res.locals.sessionLocation = geoLocation;
   return next();
 };
