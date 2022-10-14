5110d930cdd98aafa35dad6135538a956ae4422c
Saurabh Patel <saurabhp@masteroapp.com>
2022-10-14 19:45:07 +0530

    feat: exported authentication middleware

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './helpers/jwt';
 export * from './helpers/logger';
 export * from './middlewares/sessionLocation'
 export * from './middlewares/validator'
+export * from './middlewares/authentication'
 export * from './wrappers/natsWrapper'
 export * from './wrappers/redisWrapper'
 export * from './eventBus/baseListener'
