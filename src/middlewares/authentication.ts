394d4fd4e19a579cc6b61887821aeb6099a03463
Saurabh Patel <saurabhp@masteroapp.com>
2022-10-18 23:03:33 +0530

    feat(authentication): updated authentication middleware to throw error on unauthenticated request

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
-import { AuthFailureError } from '../errors';
+import { AccessTokenError, AuthFailureError } from '../errors';
 import { JwtPayload } from '../helpers/jwt';
 
 declare global {
@@ -14,16 +14,16 @@ declare global {
 
 export const authentication = (publicKey: string) =>
   async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.session?.accessToken) return next();
-    if (!req.session?.userId) return next();
-    if (!req.session?.username) return next();
+    if (!req.session.accessToken) throw new AuthFailureError('Unable to find access token.')
+    if (!req.session.userId) throw new AuthFailureError('Unable to find user Id.')
+    if (!req.session.username) throw new AuthFailureError('Unable to find user name')
 
     const payload = verify(req.session.accessToken, publicKey) as JwtPayload;
 
     if (!payload || !payload.iss || !payload.aud || !payload.prm
       || payload.username !== req.session.username
       || payload.userId !== req.session.userId)
-      throw new AuthFailureError('Invalid Access Token');
+      throw new AccessTokenError('Invalid Access Token');
 
     res.locals.payload = payload;
     res.locals.username = req.session.username;
