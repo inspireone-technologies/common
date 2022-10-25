f3d9ad69908cddd650ffd045b0b1cec3a200a326
Saurabh Patel <saurabhp@masteroapp.com>
2022-10-25 19:01:34 +0530

    feat(logger): added aws-cloudwatch logger transport for productions

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -2,6 +2,8 @@ import { createLogger, transports, format } from 'winston';
 import fs from 'fs';
 import path from 'path';
 import DailyRotateFile from 'winston-daily-rotate-file';
+// @ts-ignore
+import winstonCloudWatch from 'winston-aws-cloudwatch';
 
 let dir = process.env.LOG_DIR;
 if (!dir) dir = path.resolve('logs');
@@ -31,16 +33,41 @@ const options = {
 	}
 };
 
+const loggerTransports = [
+	new transports.Console({
+		level: logLevel,
+		format: format.combine(
+			format.errors({ stack: true }),
+			format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+			format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`),
+			format.prettyPrint(),
+		)
+	})
+]
+
+if (process.env.NODE_ENV === 'production') {
+	if (!process.env.AWS_LOG_GROUP_NAME) throw new Error('AWS_LOG_GROUP_NAME must be defined');
+	if (!process.env.AWS_LOG_STREAM_NAME) throw new Error('AWS_LOG_STREAM_NAME must be defined');
+	if (!process.env.AWS_ACCESS_KEY_ID) throw new Error('AWS_ACCESS_KEY_ID must be defined');
+	if (!process.env.AWS_SECRET_ACCESS_KEY) throw new Error('AWS_SECRET_ACCESS_KEY must be defined');
+	if (!process.env.AWS_REGION) throw new Error('AWS_REGION must be defined');
+
+	loggerTransports.push(
+		new winstonCloudWatch({
+			logGroupName: process.env.AWS_LOG_GROUP_NAME,
+			logStreamName: process.env.AWS_LOG_STREAM_NAME,
+			createLogGroup: true,
+			createLogStream: true,
+			awsConfig: {
+				accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+				secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+				region: process.env.AWS_REGION
+			}
+		}))
+}
+
 export const logger = createLogger({
-	transports: [
-		new transports.Console({
-			level: logLevel,
-			format: format.combine(
-				format.errors({ stack: true }),
-				format.prettyPrint()
-			)
-		}),
-	],
+	transports: loggerTransports,
 	exceptionHandlers: [
 		new DailyRotateFile(options.file),
 	],
