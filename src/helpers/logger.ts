969cbe73c0009d3b74edab9cb19ea86da96f2e98
Saurabh Patel <saurabhp@masteroapp.com>
2022-10-31 23:13:13 +0530

    feat(logger): removed cloudwatch from logger

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -2,7 +2,6 @@ import { createLogger, transports, format } from 'winston';
 import fs from 'fs';
 import path from 'path';
 import DailyRotateFile from 'winston-daily-rotate-file';
-import WinstonCloudWatch from 'winston-cloudwatch';
 
 let dir = process.env.LOG_DIR;
 if (!dir) dir = path.resolve('logs');
@@ -15,21 +14,14 @@ if (!fs.existsSync(dir)) {
 
 const logLevel = process.env.NODE_ENV === 'development' ? 'debug' : 'warn';
 
-const options = {
-	file: {
-		level: logLevel,
-		filename: dir + '/%DATE%.log',
-		datePattern: 'YYYY-MM-DD',
-		zippedArchive: true,
-		timestamp: true,
-		handleExceptions: true,
-		humanReadableUnhandledException: true,
-		prettyPrint: true,
-		json: true,
-		maxSize: '20m',
-		colorize: true,
-		maxFiles: '14d'
-	}
+const rotateOptions = {
+	level: 'error',
+	filename: dir + '/%DATE%.log',
+	datePattern: 'YYYY-MM-DD',
+	zippedArchive: true,
+	timestamp: true,
+	maxSize: '20m',
+	maxFiles: '14d'
 };
 
 const consoleTransports = new transports.Console({
@@ -43,32 +35,8 @@ const consoleTransports = new transports.Console({
 
 const winstonLogger = createLogger({
 	transports: consoleTransports,
-	exceptionHandlers: [new DailyRotateFile(options.file)],
+	exceptionHandlers: [new DailyRotateFile(rotateOptions)],
 	exitOnError: false
 });
-
-
-if (process.env.NODE_ENV === 'production') {
-	if (!process.env.AWS_LOG_GROUP_NAME) throw new Error('AWS_LOG_GROUP_NAME must be defined');
-	if (!process.env.AWS_LOG_STREAM_NAME) throw new Error('AWS_LOG_STREAM_NAME must be defined');
-	if (!process.env.AWS_ACCESS_KEY_ID) throw new Error('AWS_ACCESS_KEY_ID must be defined');
-	if (!process.env.AWS_SECRET_ACCESS_KEY) throw new Error('AWS_SECRET_ACCESS_KEY must be defined');
-	if (!process.env.AWS_REGION) throw new Error('AWS_REGION must be defined');
-
-	winstonLogger.add(
-		new WinstonCloudWatch({
-			awsOptions: {
-				credentials: {
-					accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-					secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-				},
-				region: process.env.AWS_REGION
-			},
-			logGroupName: process.env.AWS_LOG_GROUP_NAME,
-			logStreamName: process.env.AWS_LOG_STREAM_NAME,
-			jsonMessage: true
-		})
-	)
-}
 
 export const logger = winstonLogger;
