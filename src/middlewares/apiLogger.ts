3a999a21b382f3dfdbfe3cbe5f20107af7939e08
Saurabh Patel <saurabhp@masteroapp.com>
2022-10-31 23:13:33 +0530

    feat(apilogger): added error logger

--- src/middlewares/apiLogger.ts
+++ src/middlewares/apiLogger.ts
@@ -5,7 +5,13 @@ export const apiLogger = expressWinston.logger({
   winstonInstance: logger,
   meta: false,
   expressFormat: true,
-  colorize: true,
+  colorize: false,
   statusLevels: true,
   skip: (req, _res) => req.method === 'OPTIONS'
+})
+
+export const apiErrorLogger = expressWinston.errorLogger({
+  winstonInstance: logger,
+  meta: false,
+  level: 'error',
 })
