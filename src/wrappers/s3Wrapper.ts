43651d64d81827fa05b3da6062db401264968c97
Saurabh Patel <saurabhp@masteroapp.com>
2022-12-03 16:36:47 +0530

    feat(s3): added s3 wrapper for s3 client operations

---
+++ src/wrappers/s3Wrapper.ts
@@ -0,0 +1,22 @@
+import { S3Client, S3ClientConfig, waitUntilBucketExists } from '@aws-sdk/client-s3';
+
+export class s3Wrapper {
+  private _client?: S3Client;
+
+  get client() {
+    if (!this._client) throw new Error('Cannot access s3 client before connecting');
+    return this._client;
+  }
+
+  connect(config: S3ClientConfig, Bucket: string, maxWaitTime: number = 1000) {
+    return new Promise<void>(async (resolve, reject) => {
+      try {
+        this._client = new S3Client(config);
+        await waitUntilBucketExists({ client: this._client, maxWaitTime }, { Bucket });
+        resolve()
+      } catch (error) {
+        reject(error)
+      }
+    });
+  }
+}
