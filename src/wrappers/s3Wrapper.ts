017eb4dffe2c6e040c36a92c6ad51586707462fa
Saurabh Patel <saurabhp@masteroapp.com>
2022-12-07 14:34:31 +0530

    feat(s3): exported s3 library for calling sub commands

--- src/wrappers/s3Wrapper.ts
+++ src/wrappers/s3Wrapper.ts
@@ -1,4 +1,5 @@
 import { S3Client, S3ClientConfig, waitUntilBucketExists } from '@aws-sdk/client-s3';
+import * as s3Library from '@aws-sdk/client-s3';
 
 export class s3Wrapper {
   private _client?: S3Client;
@@ -6,6 +7,10 @@ export class s3Wrapper {
   get client() {
     if (!this._client) throw new Error('Cannot access s3 client before connecting');
     return this._client;
+  }
+
+  get s3Library() {
+    return s3Library
   }
 
   connect(config: S3ClientConfig, Bucket: string, maxWaitTime: number = 1000) {
