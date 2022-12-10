e4418b46c4ec4713be12ce6cfe4322465b2ff835
Saurabh Patel <saurabhp@masteroapp.com>
2022-12-10 10:52:27 +0530

    feat(s3): changed s3 client for s3wrapper

--- src/wrappers/s3Wrapper.ts
+++ src/wrappers/s3Wrapper.ts
@@ -1,8 +1,8 @@
-import { S3Client, S3ClientConfig, waitUntilBucketExists } from '@aws-sdk/client-s3';
+import { S3, S3ClientConfig, waitUntilBucketExists } from '@aws-sdk/client-s3';
 import * as s3Library from '@aws-sdk/client-s3';
 
 export class s3Wrapper {
-  private _client?: S3Client;
+  private _client?: S3;
 
   get client() {
     if (!this._client) throw new Error('Cannot access s3 client before connecting');
@@ -16,7 +16,7 @@ export class s3Wrapper {
   connect(config: S3ClientConfig, Bucket: string, maxWaitTime: number = 1000) {
     return new Promise<void>(async (resolve, reject) => {
       try {
-        this._client = new S3Client(config);
+        this._client = new S3(config);
         await waitUntilBucketExists({ client: this._client, maxWaitTime }, { Bucket });
         resolve()
       } catch (error) {
