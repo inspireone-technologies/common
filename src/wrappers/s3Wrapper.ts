24cb70810b8688e2b60f90c46b4180dca82fad30
Saurabh Patel <saurabhp@masteroapp.com>
2023-01-19 02:52:03 +0530

    feat(s3wrapper): updated s3 wrapper to get multiple bucket at once

--- src/wrappers/s3Wrapper.ts
+++ src/wrappers/s3Wrapper.ts
@@ -1,27 +1,31 @@
 import { S3, S3ClientConfig, waitUntilBucketExists } from '@aws-sdk/client-s3';
 import * as s3Library from '@aws-sdk/client-s3';
 
-export class s3Wrapper {
-  private _client?: S3;
+interface Buckets {
+  [key: string]: string;
+}
 
-  get client() {
-    if (!this._client) throw new Error('Cannot access s3 client before connecting');
-    return this._client;
-  }
+export class s3Wrapper {
+  private _clients = new Map<string, S3>();
 
   get s3Library() {
     return s3Library
   }
 
-  connect(config: S3ClientConfig, Bucket: string, maxWaitTime: number = 1000) {
-    return new Promise<void>(async (resolve, reject) => {
-      try {
-        this._client = new S3(config);
-        await waitUntilBucketExists({ client: this._client, maxWaitTime }, { Bucket });
-        resolve()
-      } catch (error) {
-        reject(error)
+  async connect(config: S3ClientConfig, buckets: Buckets, maxWaitTime: number = 1000) {
+    try {
+      for (const [bucketName, bucket] of Object.entries(buckets)) {
+        const s3 = new S3(config);
+        await waitUntilBucketExists({ client: s3, maxWaitTime }, { Bucket: bucket });
+        this._clients.set(bucketName, s3);
       }
-    });
+    } catch (error) {
+      throw new Error(`Error connecting to one or more buckets. ${error}`);
+    }
+  }
+
+  getClient(bucketName: string) {
+    if (!this._clients.has(bucketName)) throw new Error(`Cannot access s3 client for ${bucketName} before connecting`);
+    return this._clients.get(bucketName);
   }
 }
