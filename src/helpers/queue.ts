038b68e89c2c788c3ff4811d137fd04ae5018345
Saurabh Patel <saurabhp@masteroapp.com>
2023-01-24 17:15:24 +0530

    feat(bull): added getQueue function to get bull queue by name

--- src/helpers/queue.ts
+++ src/helpers/queue.ts
@@ -1,5 +1,14 @@
 import Bull, { QueueOptions, Queue } from 'bull';
 
-export const bullQueue = (queueName: string, options: QueueOptions): Queue => {
-  return new Bull(queueName, options)
+const queues = new Map();
+
+export const bullQueue = (queueName: string, options: QueueOptions = {}): Queue => {
+  const queue = new Bull(queueName, options);
+  queues.set(queueName, queue);
+  return queue;
+}
+
+export const getQueue = (queueName: string, options: QueueOptions = {}): Queue => {
+  if (!queues.has(queueName)) queues.set(queueName, bullQueue(queueName, options));
+  return queues.get(queueName);
 }
