bfab288e2084d1ce1969a632edab00cf949326c9
Saurabh Patel <saurabhp@masteroapp.com>
2023-04-25 12:20:46 +0530

    feat(jwt): updates jwt payload

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -76,7 +76,7 @@ type Payload = {
 	userId: string,
 	username: string,
 	companyId?: string,
-	sessionId?: string,
+	tokenId?: string,
 	param: string,
 	validity: number
 }
@@ -91,18 +91,18 @@ export class JwtPayload {
 	userId: string;
 	username: string;
 	companyId: string;
-	sessionId: string;
+	tokenId: string;
 	iat: number;
 	exp: number;
 	prm: string;
 
-	constructor({ issuer, audience, userId, username, companyId, sessionId, param, validity }: Payload) {
+	constructor({ issuer, audience, userId, username, companyId, tokenId, param, validity }: Payload) {
 		this.iss = issuer;
 		this.aud = audience;
 		this.userId = userId;
 		this.username = username;
 		this.companyId = companyId || 'undefined';
-		this.sessionId = sessionId || 'undefined';
+		this.tokenId = tokenId || 'undefined';
 		this.iat = Math.floor(Date.now() / 1000);
 		this.exp = this.iat + (validity * HOURS_IN_DAY * MINUTES_IN_HOUR * SECONDS_IN_MINUTES);
 		this.prm = param;
