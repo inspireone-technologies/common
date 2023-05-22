f18b934768053c32f701d1df6f0c71f1b3bee7a5
Saurabh Patel <saurabhp@masteroapp.com>
2023-05-23 01:01:54 +0530

    feat(errorlogger): adds error logger & change apilogger transport mode to console

--- src/middlewares/apiLogger.ts
+++ src/middlewares/apiLogger.ts
@@ -1,11 +1,22 @@
-import { logger } from './../helpers/logger';
+import { transports, format } from 'winston';
 import expressWinston from 'express-winston'
 
 export const apiLogger = expressWinston.logger({
-  winstonInstance: logger,
+  transports: [new transports.Console()],
   meta: false,
   expressFormat: true,
   colorize: false,
   statusLevels: true,
+  skip: (req, _res) => req.method === 'OPTIONS'
+})
+
+export const errorLogger = expressWinston.errorLogger({
+  transports: [new transports.Console()],
+  format: format.combine(
+    format.errors({ stack: true }),
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.json()
+  ),
+  dynamicMeta: (req, res, _err) => { return { body: req.body, locals: res.locals, headers: req.headers } },
   skip: (req, _res) => req.method === 'OPTIONS'
 })
