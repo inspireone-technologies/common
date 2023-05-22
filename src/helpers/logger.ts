93bcec8a16b5175bfade0e0e536f0ab7a732c538
Saurabh Patel <saurabhp@masteroapp.com>
2023-05-23 00:59:15 +0530

    feat(logger): updates winston logger, removed DailyRotateFile & used console format json

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,42 +1,19 @@
 import { createLogger, transports, format } from 'winston';
-import fs from 'fs';
-import path from 'path';
-import DailyRotateFile from 'winston-daily-rotate-file';
-
-let dir = process.env.LOG_DIR;
-if (!dir) dir = path.resolve('logs');
-
-// create directory if it is not present
-if (!fs.existsSync(dir)) {
-	// Create the directory if it does not exist
-	fs.mkdirSync(dir);
-}
 
 const logLevel = process.env.NODE_ENV === 'production' ? 'verbose' : 'debug';
-
-const rotateOptions = {
-	level: 'error',
-	json: true,
-	filename: dir + '/%DATE%.log',
-	datePattern: 'YYYY-MM-DD',
-	zippedArchive: true,
-	timestamp: true,
-	maxSize: '20m',
-	maxFiles: '14d'
-};
 
 const consoleTransports = new transports.Console({
 	level: logLevel,
 	format: format.combine(
 		format.errors({ stack: true }),
 		format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-		format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`),
+		format.json()
 	)
 })
 
 const winstonLogger = createLogger({
 	transports: consoleTransports,
-	exceptionHandlers: [new DailyRotateFile(rotateOptions)],
+	exceptionHandlers: consoleTransports,
 	exitOnError: false
 });
 
