d9a1dac034b03be4bdb43512d38f771e08220f01
Saurabh Patel <saurabhp@masteroapp.com>
2023-05-23 03:22:40 +0530

    feat(error-logger): updates error logger to add error logs to pm2

--- src/middlewares/apiLogger.ts
+++ src/middlewares/apiLogger.ts
@@ -3,6 +3,14 @@ import expressWinston from 'express-winston'
 
 export const apiLogger = expressWinston.logger({
   transports: [new transports.Console()],
+  format: format.combine(
+    format.errors({ stack: true }),
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf(({ level, message, timestamp, stack }) => {
+      if (stack) return `${timestamp} ${level}: ${message}\n${stack}`;// Custom format for error logs
+      return `${timestamp} ${level}: ${message}`;// Default format for other logs
+    })
+  ),
   meta: false,
   expressFormat: true,
   colorize: false,
@@ -15,8 +23,15 @@ export const errorLogger = expressWinston.errorLogger({
   format: format.combine(
     format.errors({ stack: true }),
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    format.json()
-  ),
-  dynamicMeta: (req, res, _err) => { return { body: req.body, locals: res.locals, headers: req.headers } },
+    format.printf(({ level, message, timestamp, meta }) => {
+      const error = JSON.stringify(meta?.error);
+      const payload = JSON.stringify(meta?.payload);
+      console.error({ timestamp, level, message, meta });
+      return `${timestamp} ${level}: ${message}, payload: ${payload}, error: ${error}`;
+    })),
+  meta: true,
+  requestField: null,
+  blacklistedMetaFields: ['process', 'stack', 'trace', 'os', 'message'],
+  dynamicMeta: (req, res, _err) => { return { body: req.body, payload: res.locals?.payload, sessionLocation: res.locals?.sessionLocation }; },
   skip: (req, _res) => req.method === 'OPTIONS'
 })
