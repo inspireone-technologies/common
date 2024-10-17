e6628af2505653d5bc71071e9ba49c8213c82c65
mayankrajput8745 <mayankr@masteroapp.com>
2024-10-17 13:09:56 +0530

    Table name is changed to search to searches

--- src/helpers/nanoId.ts
+++ src/helpers/nanoId.ts
@@ -76,5 +76,5 @@ export enum prefixes {
   ManagerToken = 'matok_',
   ManagerSession = 'mases_',
   UserMicroskillChallengeSkill = 'usmcs_',
-  UserRecentSearch = 'usres_',
+  UserRecentSearches = 'usres_',
 }
